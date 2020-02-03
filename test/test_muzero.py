4f56564dbcfcd149938537707ecd0cbdb66ffce7
radiachk <Ra21022000>
2020-02-03 02:44:32 +0100

    Added first test file

---
+++ test/test_muzero.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestMuZero(TestCase):
+    def test_populate_replay_buffer(self):
+        self.fail()
+
+    def test_play_game(self):
+        self.fail()
+
+    def test_train_network(self):
+        self.fail()
+
+    def test_update_weights(self):
+        self.fail()
+
+    def test_scalar_loss(self):
+        self.fail()
+
+    def test_evaluate(self):
+        self.fail()
